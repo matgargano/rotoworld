d1e13d330309ca826ec12ce3df152fd2a5cafb17
Mat Gargano <mgargano@gmail.com>
2015-09-02 08:43:58 -0400

    convert praseData to a static method, add ability to choose a sport, add a typehint for parseData, update comments

--- src/Rotoworld.php
+++ src/Rotoworld.php
@@ -22,6 +22,8 @@ with this anyhow and anywhere.
 */
 
 namespace Rotoworld;
+use SimpleHtmlDom\simple_html_dom_node;
+
 /**
  * Class Rotoworld News
  */
@@ -35,23 +37,27 @@ class Rotoworld {
 
 
   /**
-   * @param string $url
-   * @param string $itemIdentifier
+   * @param string $sport accepts the endpoint after the baseUrl from rotoworld.com, accepts mlb, nfl, nhl, bpl, gol and nas
+   * @param string $baseUrl this should never change, but it is the base url + uri for scraping the data
+   * @param string $itemIdentifier this should never change, this is the sizzle style selector for grabbing each block of news
    */
-  public function __construct( $url = 'http://www.rotoworld.com/sports/mlb/baseball', $itemIdentifier = '.pb' ) {
-    $this->url            = $url;
+  public function __construct( $sport = 'mlb', $baseUrl = 'http://www.rotoworld.com/sports/', $itemIdentifier = '.pb' ) {
+
+    $this->url            = $baseUrl . $sport;
     $this->itemIdentifier = $itemIdentifier;
   }
 
 
   /**
+   * Grab and return the data
+   *
    * @return array
    */
   public function get() {
     $dataArray = array();
     $html      = \SimpleHtmlDom\file_get_html( $this->url );
     foreach ( $html->find( $this->itemIdentifier ) as $element ) {
-      $data        = $this->parseData( $element );
+      $data        = self::parseData( $element );
       $dataArray[] = $data;
     }
 
@@ -66,8 +72,9 @@ class Rotoworld {
    *
    * @return \stdClass
    */
-  private function parseData( $element ) {
+  private static function parseData( simple_html_dom_node $element ) {
     $data = (object) array();
+    /* @var $elementContent simple_html_dom_node */
     foreach ( $element->find( '.headline, .report, .impact, .info' ) as $elementContent ) {
       $class = $elementContent->attr['class'];
       switch ( $class ) {
